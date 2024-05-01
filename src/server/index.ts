34ddd49da781e767dcdd8abb077a9bb7e3d3c8a5
Rokas Rudgalvis <rokas@rudgalvis.com>
2024-05-01 17:43:27 +0200

    chore: dev env

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,7 +5,7 @@ import { getSidebarJson, itemsData, spacesData, tabsData } from "./parse-arc";
 
 // Create Express server
 const server = express();
-const port = 55513; // random port representing JJGLE where 5 is J & G, 1 is L, 3 is E
+const port = process.env.PORT || 55513; // random port representing JJGLE where 5 is J & G, 1 is L, 3 is E
 
 // Use cors middleware
 server.use(cors());
@@ -32,6 +32,18 @@ server.get('/tab/:tabId', async (req, res) => {
         return res.json({error: 'No space found'})
     }
 });
+
+server.get('/dev', async (req, res) => {
+    const data = await getSidebarJson();
+
+    const spaces = spacesData(data);
+    const items = itemsData(data);
+    const tabs = tabsData(items, spaces);
+
+//    const space = tabs.filter(e => e.tabId === +req.params.tabId);
+
+    res.json(data)
+})
 
 export const startServer = () => server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
