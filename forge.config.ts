26d894a19f3792040bdadbdff8987c570e6b7758
armandokun <armandasvaicikauskas@protonmail.com>
2025-02-05 13:34:09 +0200

    Add Apple signatures

--- forge.config.ts
+++ forge.config.ts
@@ -1,78 +1,94 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { VitePlugin } from '@electron-forge/plugin-vite';
-import { FusesPlugin } from '@electron-forge/plugin-fuses';
-import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
+import { VitePlugin } from "@electron-forge/plugin-vite";
+import { FusesPlugin } from "@electron-forge/plugin-fuses";
+import { FuseV1Options, FuseVersion } from "@electron/fuses";
 
 const config: ForgeConfig = {
-    packagerConfig: {
-        asar: true,
-        icon: './assets/mac-icon', // no file extension required,
+  packagerConfig: {
+    asar: true,
+    icon: "./assets/mac-icon", // no file extension required
+    appBundleId: process.env.APP_BUNDLE_ID,
+    extendInfo: {
+      LSMinimumSystemVersion: "12.0.0",
     },
-    rebuildConfig: {},
-    makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({}),
-        {
-            name: '@electron-forge/maker-dmg',
-            config: {
-                overwrite: true,
-                name: "JustJuggle-ArcBridge-beta",
-                "icon-size": 80,
-                debug: true,
-                background: './assets/dmg-background-2.png',
-                format: 'ULFO',
-                additionalDMGOptions: {
-                    "icon-size": 3000,
-                    window: {
-                        size: {
-                            width: 856,
-                            height: 500
-                        },
-                        position: {
-                            x: 500,
-                            y: 500
-                        }
-                    },
-                }
+    osxSign: {
+      identity: process.env.APPLE_SIGNING_IDENTITY,
+    },
+    osxNotarize: {
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    },
+  },
+  rebuildConfig: {},
+  makers: [
+    new MakerSquirrel({}),
+    new MakerZIP({}, ["darwin"]),
+    new MakerRpm({}),
+    new MakerDeb({}),
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        overwrite: true,
+        name: "JustJuggle-ArcBridge-beta",
+        "icon-size": 80,
+        debug: true,
+        background: "./assets/dmg-background-2.png",
+        format: "ULFO",
+        additionalDMGOptions: {
+          "icon-size": 3000,
+          window: {
+            size: {
+              width: 856,
+              height: 500,
             },
-        }
-    ],
-    plugins: [
-        new VitePlugin({
-            // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-            // If you are familiar with Vite configuration, it will look really familiar.
-            build: [
-                {
-                    // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-                    entry: 'src/main.ts',
-                    config: 'vite.main.config.ts',
-                },
-                {
-                    entry: 'src/preload.ts',
-                    config: 'vite.preload.config.ts',
-                },
-            ],
-            renderer: [
-                {
-                    name: 'main_window',
-                    config: 'vite.renderer.config.ts',
-                },
-            ],
-        }),
-        // Fuses are used to enable/disable various Electron functionality
-        // at package time, before code signing the application
-        new FusesPlugin({
-            version: FuseVersion.V1,
-            [FuseV1Options.RunAsNode]: false,
-            [FuseV1Options.EnableCookieEncryption]: true,
-            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-            [FuseV1Options.EnableNodeCliInspectArguments]: false,
-            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-            [FuseV1Options.OnlyLoadAppFromAsar]: true,
-        }),
-    ],
+            position: {
+              x: 500,
+              y: 500,
+            },
+          },
+        },
+      },
+    },
+  ],
+  plugins: [
+    new VitePlugin({
+      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+      // If you are familiar with Vite configuration, it will look really familiar.
+      build: [
+        {
+          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+          entry: "src/main.ts",
+          config: "vite.main.config.ts",
+        },
+        {
+          entry: "src/preload.ts",
+          config: "vite.preload.config.ts",
+        },
+      ],
+      renderer: [
+        {
+          name: "main_window",
+          config: "vite.renderer.config.ts",
+        },
+      ],
+    }),
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
 };
 
 export default config;
